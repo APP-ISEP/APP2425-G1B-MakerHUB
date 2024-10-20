0120b6167cc4b737eadfc649fc1956fe141d224a
savacartonner <victor.munerot@hotmail.com>
2024-10-20 13:02:05 +0100

    [FIX] fix css

--- assets/components/header/NavBar.js
+++ assets/components/header/NavBar.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#pseudo").click(function() {
+    $("#right_elements").click(function() {
         $("#dropdown").stop(true, true).slideToggle("slow");
     });
 });
