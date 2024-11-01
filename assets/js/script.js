2906c94f7e1df2183d0cde321d0dd477de8dee17
Mathieu GEISSEL <137196499+mgeissel@users.noreply.github.com>
2024-11-01 13:02:14 +0100

    [PULL REQUEST] Edit profile page (#4)
    * [FEAT] Start edit profile page
    
    * [FEAT] Feat edit profile static page
    
    * [FIX] Fix filename and responsive design
    
    * [FIX] Last fix before pull request
    
    * [FIX] Added PHP to allow usage of components
    
    Fixed some max-length inside inputs
    Fixed some CSS
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>
    
    * Update edit-profile.html
    
    Removed what's inside footer.html
    
    * [FIX] Fix form field
    
    * [FIX] Removed required from phone number
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>
    
    ---------
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>
    Co-authored-by: Morgane Breton <morgane4478@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ $(document).ready(function() {
         $("#dropdown").toggle()
     });
 });
+
+
+document.getElementById('toggleDescription').addEventListener('change', function () {
+    const descriptionSection = document.getElementById('descriptionSection');
+    descriptionSection.style.display = this.checked ? 'flex' : 'none';
+});
