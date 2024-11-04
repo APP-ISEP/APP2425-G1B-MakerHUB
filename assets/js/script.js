fe9b76b587562c75867d87df01f0ddfb629b5dfa
Morgane Breton <morgane4478@gmail.com>
2024-11-04 21:49:57 +0100

    [PULL REQUEST] Log-in page (fullstack) (#9)
    * [FEAT] Beginning of front-end
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>
    
    * [FEAT] Beginning of back-end
    
    Added PDO
    Added environnement variables
    Protection against SQL injections
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>
    
    * [FIX] Syntax fixes
    [FEAT] Beginning of adding dynamic pseudo on navbar
    [FEAT] Added logout button, unsetting the session
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>
    
    * [WIP] Added dynamic navbar
    Added hashed / dehashed password (algo :PASSWORD_DEFAULT = "2y")
    
    * [FEAT] Added front-end to login page
    Update of db credentials
    Added the 2 buttons (signup / login) inside navbar for unauthenticated users
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>
    
    * [FIX] Footer position depending of container height
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>
    
    ---------
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,29 @@ $(document).ready(function() {
     });
 });
 
+//--------- BEGINNING EYE BUTTON TO SEE PASSWORD---------//
+function show() {
+    var p = document.getElementById('password');
+    p.setAttribute('type', 'text');
+}
+
+function hide() {
+    var p = document.getElementById('password');
+    p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    }
+}, false);
+//--------- END EYE BUTTON TO SEE PASSWORD---------//
 
 document.getElementById('toggleDescription').addEventListener('change', function () {
     const descriptionSection = document.getElementById('descriptionSection');
