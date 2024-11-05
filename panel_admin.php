aaf10f7aa58d25ed29adf2e213c31d28dc307466
harpeliot <94528044+harpeliot@users.noreply.github.com>
2024-11-05 16:30:18 +0100

    panel_admin.php ready to review before merge
    Ajout du CSS spécifique au panel dans un fichier dédié styl_admin.css pour éviter les conflits
    Panel admin avec possibilité de :
    - supprimer un produit
    - supprimer un utilisateur
    - supprimer les FAQ
    - Ajouter les FAQ

--- panel_admin.php
+++ panel_admin.php
@@ -2,6 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
+        <link href="assets/css/style_admin.css" rel="stylesheet">
         <link href="assets/css/style.css" rel="stylesheet">
         <link href="assets/images/favico.ico" rel="icon">
         <title>Administration | Makerhub</title>
@@ -15,21 +16,21 @@
 
         <div class="profil" id="profil">
             <img alt="Photo de profil" class="img-profil" src="assets/images/profil.svg">
-            <p id="pseudo">Pseudonyme</p>
+            <p id="pseudo">Panel</p>
 
             <div id="dropdown">
                 <a href="#faq" ><div class="drop-content">FAQ</div></a>
-                <a href="#supprimer-produit" ><div class="drop-content">Supprimer produit</div></a>
-                <a href="#supprimer-utilisateur" ><div class="drop-content">Supprimer utilisateur</div></a>
+                <a href="#supprimer-produit" ><div class="drop-content">Produit</div></a>
+                <a href="#supprimer-utilisateur" ><div class="drop-content">Utilisateur</div></a>
                 <hr id="separation-bar">
                 <a href="log-out.php" ><div class="drop-content logout">Se déconnecter</div></a>
             </div>
         </div>
     </div>
 
-   <main>
+   <main class="main">
 
-        <section id="faq" class="content-box">
+        <section id="faq" class="section content-box">
            
                     <h2>FAQ</h2>
                     <p>Vous pouvez ajouter ou supprimer des questions fréquemment posées.</p>
@@ -37,13 +38,13 @@
                     <form action="faq.php" method="POST">
                         <label for="question">Question</label>  
                         <br>  
-                        <input type="text" name="question" placeholder="Question">
+                        <textarea type="text" name="question" placeholder="Question"></textarea>
                         <br>
                         <label for="reponse">Réponse</label>
                         <br>
-                        <input type="text" name="reponse" placeholder="Réponse">
+                        <textarea type="text" name="reponse" placeholder="Réponse"></textarea>
                         <br>
-                        <input type="submit" value="Ajouter">
+                        <input class="submit" type="submit" value="Ajouter">
                     </form>
 
                     <table>
@@ -62,16 +63,31 @@
                             </tr>
                         <?php } ?>
                         --->
+                        <tr>
+                            <td>Question 1</td>
+                            <td>Réponse 1</td>
+                            <td><a href=""><button class="submit">Supprimer</button></a></td>
+                        </tr>
+                        <tr>
+                            <td>Question 2</td>
+                            <td>Réponse 2</td>
+                            <td><a href=""><button class="submit">Supprimer</button></a></td>
+                        </tr>
+                        <tr>
+                            <td>Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3Question 3</td>
+                            <td>Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3Réponse 3</td>
+                            <td><a href=""><button class="submit">Supprimer</button></a></td>
+                        </tr>
                     </table>
 
                 </section>
 
 
-                <section id="supprimer-produit" class="content-box">
+                <section id="supprimer-produit" class="section content-box">
                     <h2>Produits</h2>
-                    <p>Vous pouvez supprimer des produits.</p>
-                    <form action="supprimer_produit.php" method="POST">
-                        <select>
+                    <form class="form-container" action="supprimer_produit.php" method="POST">
+                    <label for="suppression_produit">Supprimer un produit</label>
+                    <select name="suppression_produit" autocomplete="on">
                             <option value=""> -- Sélectionner un produit -- </option>
                             <?php foreach($produits as $produit) {
                                 $id_produit = $produit['id_produit'];
@@ -79,28 +95,29 @@
                                 <option value="<?php echo($id_produit);?>"><?php echo($nom_produit);?></option>
                             <?php } ?>
                         </select>
-                        <input type="submit" value="Supprimer">
+                        <input class="submit" type="submit" value="Supprimer">
                     </form>
                 </section>
 
 
-                <section id="supprimer-utilisateur" class="content-box">
+                <section id="supprimer-utilisateur" class="section content-box">
                     <h2>Utilisateurs</h2>
-                    <p>Vous pouvez supprimer des utilisateurs.</p>
-                    <form action="supprimer_utilisateur.php" method="POST">
-                        <select>
+                    <form class="form-container" action="supprimer_utilisateur.php" method="POST">
+                        <label for="suppression_utilisateur">Supprimer un utilisateur</label>
+                        <select name="suppression_utilisateur" autocomplete="on">
                             <option value=""> -- Sélectionner un utilisateur -- </option>
                             <?php foreach($utilisateurs as $utilisateur) {
                                 $id_utilisateur = $utilisateur['id_utilisateur'];
                                 $email_utilisateur = $utilisateur['email'];?>
                                 <option value="<?php echo($id_utilisateur);?>"><?php echo($email_utilisateur);?></option>
                             <?php } ?>
+                            <option value="1">test</option>
+
                         </select>
-                        <input type="submit" value="Supprimer">
+                        <input class="submit"  type="submit" value="Supprimer">
                     </form>
                 </section>
-    </main>
-    </body> 
+    </main> 
 
 <?php
 include_once 'views/components/footer.html';
