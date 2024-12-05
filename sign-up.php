4d9b498abb389220ad5cac4797f22eb7a341a9ab
Sere04 <155636492+Sere04@users.noreply.github.com>
2024-12-05 19:57:10 +0100

    mise à jour back end

--- sign-up.php
+++ sign-up.php
@@ -8,14 +8,11 @@ ob_start();
 include_once 'views/sign-up.html';
 require_once 'controleur/SignUpControleur.php'
 require_once 'modele/SignUpModele.php';
-require_once "modele/connexionDB.php";
+include_once "modele/connexionDB.php"; //essayé aussi avec require once
 require_once "modele/SignUpFunction.php";
 
 $telephone ="0612345678";
 $nom = $email = $telephone = $prenom = $motDePasse =$pseudonyme =  "";
-
-
-conncectToDB(); // à  ce niveau là il faut placer la connexion VERIFIER
 
 if (isset($_POST['submit'])) {
   $nom = test_input($_POST["nom"]); //redondance ???
@@ -34,20 +31,20 @@ if (isset($_POST['submit'])) {
       $hashedPassword = HashPassword($motDePasse);
       $result = InsertUser(string $nom , string $prenom, string $pseudonyme, string $email, string $motDePasse, string $telephone);
       if ($result){
-      echo "Tu viens de t'inscrire comme".$pseudonyme;
+      echo "<p>Tu viens de t'inscrire comme $pseudonyme.</p>";
     } else{
-      $errors['inscription'] ="Error data inserting.</br>";
+      $errors['inscription'] = "Erreur lors de l' insertion des données.";
   
     }
     catch (PDOException $e) {
-        echo "Error inserting data: " . $e->getMessage();
+        echo "<p>Erreur : " . htmlspecialchars($e->getMessage()) . "</p>";
     }
   }
   }
 }
 $body = ob_get_clean();
 
-include_once "views/components/template.php";
+include_once 'views/components/template.php';
 
 
 
