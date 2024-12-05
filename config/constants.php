4e3b440c4a98006bc387ae570faed0415e707494
Morgane Breton <morgane4478@gmail.com>
2024-12-05 22:27:09 +0100

    [FEAT] Logs
    Added autoload for namespaces
    Added LogFile, Log, and LogLevel classes
    Added LogFileSingleton
    
    Logged each login
    Logged each time an user edits its profile
    Logged each logout
    Moved all important constants inside a special file (because cant do .env setup...)
    
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+// INCLURE CE FICHIER AU DEBUT DE CHAQUE FICHIER QUI UTILISE LES CONSTANTES DEFINIES CI-DESSOUS
+
+const LOG_FILE_PATH = 'log/makerhub.log';
+const DB_HOST = 'sql3.minestrator.com';
+const DB_NAME = 'minesr_moBQkITL';
+const DB_USER = 'minesr_moBQkITL';
+const DB_PASS = 'gObeWJtKcGGA8AvS';
+const DB_PORT = '3306';
+const DB_CHARSET = 'utf8mb4';
--- log-out.php
+++ log-out.php
@@ -1,5 +1,15 @@
 <?php
+include 'config/constants.php';
+include 'autoload.php';
+
+use Config\Log;
+use Config\LogFileSingleton;
+use Config\LogLevel;
+
 session_start();
+
+$logFile = LogFileSingleton::getInstance();
+$logFile->addLog(new Log(LogLevel::INFO, "L'utilisateur " . $_SESSION['username'] . " s'est déconnecté depuis " . $_SERVER['REMOTE_ADDR'] . "."));
 
 $_SESSION = [];
 
