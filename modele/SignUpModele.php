da32270d1f6bd410033566e1065465ec4061837b
Sere04 <155636492+Sere04@users.noreply.github.com>
2024-12-05 14:50:28 +0100

    modification html et back-end

--- modele/SignUpModele.php
+++ modele/SignUpModele.php
@@ -1,25 +1,20 @@
 <?php
 require_once "./php/connectToDB.php";
 
-function InsertUser( string $nom , string $prenom, string $pseudonyme, string $email, string $motDePasse, string $telephone)
+function InsertUser( string $nom, string $prenom, string $pseudonyme, string $email, string $motDePasse, string $telephone)
 {
     try {
         $pdo = connectToDB();
-        $sql = "INSERT INTO `utilisateur` (nom, prenom,pseudonyme, mail, mot_de_passe,telephone) VALUES (:valnom,:valprenom,:valpseudo,:valmail,:valmotPasse,:valtel)";
 
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(":valnom", $nom);
-        $stmt->bindParam(":valprenom", $prenom);
-        $stmt->bindParam(":valpseudo", $pseudonyme);
-        $stmt->bindParam(":valmail", $email);
-        $stmt->bindParam(":valmotPasse", $motDePasse);
-        $stmt->bindParam(":valtel", $telephone);
+        $nom = $_REQUEST['nom'];
+        $prenom = $_REQUEST['prenom'];
+        $pseudonyme = $_REQUEST['pseudonyme'];
+        $email = $_REQUEST['email'];
+        $motDePasse = $_REQUEST['motDePasse'];
+        $telephone = $_REQUEST['telephone'];
 
+        $sql = "INSERT INTO `utilisateur` (nom, prenom,pseudonyme, mail, mot_de_passe,telephone) VALUES ('$nom','$prenom','$pseudonyme','$email','$motDePasse','$telephone')";
 
-        $bool = $stmt->execute();
-        $stmt->closeCursor();
-
-        return $bool;
     } catch (PDOException $e) {   
         // Error executing the query
         $error = $e->getMessage();
