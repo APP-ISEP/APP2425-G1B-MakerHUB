56d86db8561b641a3ffc901a641c2a1f9e6377df
annaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <anna.kucinski@eleve.isep.fr>
2024-12-06 20:00:20 +0100

    modifs vendredi

---
+++ php/contact/createFormEntry.php
@@ -0,0 +1,11 @@
+<?php
+require_once "../connectToDB.php" //../permet de revenir en arriere d'un cran
+//once car on veut se connecter qu'une sule fois la base de donnée et require parcq on a besoin que la fonction soit executé 
+
+//$anna->test(); dans anna y'a une méthode test et on l'appelle 
+// fetchassoc = mettre resultats ds tableau 
+insert into 
+//pdo= bd
+// pdo exeecption =erreur
+connectToDB(); // là on utilise la function
+
