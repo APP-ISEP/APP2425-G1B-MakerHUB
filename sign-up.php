c90b51e8237185e5485f748e72c71ff0bc123b96
Morgane Breton <morgane4478@gmail.com>
2024-12-06 22:13:03 +0100

    [FIX] Fixes de la syntaxe et qques pbs de logiques vu avec Serena en appel
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>

--- sign-up.php
+++ sign-up.php
@@ -1,49 +1,64 @@
 <?php
+session_start();
+
 $title = "Inscription";
+$errors = array();
+$isAuthPage = true;
 
 ob_start();
 
+if (isset($_SESSION['account'])) {
+    header("Location: index.php");
+}
+
+require_once 'php/user/insertUser.php';
+require_once 'php/user/checkCredentials.php';
+require_once 'php/user/getUser.php';
+
+$nom = $email = $telephone = $prenom = $motDePasse = $pseudonyme = "";
+
+if (isset($_POST) && count($_POST) > 0) {
+    $nom = test_input($_POST["nom"]);
+    $prenom = test_input($_POST["prenom"]);
+    $motDePasse = test_input($_POST["motDePasse"]);
+    $pseudonyme = test_input($_POST["pseudonyme"]);
+    $email = test_input($_POST["email"]);
+    $telephone = test_input($_POST["telephone"]);
+
+    $validateEmail = validateEmail($email);
+    $validatePhone = validateTelephone($telephone);
+    $validatePassword = validatePassword($motDePasse);
+
+    if (!$validateEmail) {
+        $errors['email'] = "Veuillez saisir un mail valide.";
+    }
+    if (!$validatePhone) {
+        $errors['telephone'] = "Veuillez saisir un téléphone valide";
+    }
+    if (!$validatePassword) {
+        $errors['motDePasse'] = "Veuillez saisir un mot de passe valide.</br> Il doit contenir au moins :</br>"
+            . '- Un chiffre.</br>'
+            . "- Une majuscule.</br>"
+            . "- Une minuscule.</br>"
+            . "- Un caractère spécial (#?!@$%^&*-).</br>"
+            . "- Longueur minimale de 8 caractères.";
+    }
+
+    if (empty($errors)) {
+        if ($validateEmail && $validatePhone && $validatePassword) {
+            $hashedPassword = hashPassword($motDePasse);
+            $result = insertUser($nom, $prenom, $pseudonyme, $email, $hashedPassword, $telephone);
+            $account = getUser($email);
+            $_SESSION['account'] = $account;
+            $_SESSION['username'] = $account['pseudonyme'];
+
+            header("Location: index.php");
+        }
+    }
+}
+
 include_once 'views/sign-up.html';
-require_once 'modele/SignUpModele.php';
-require_once "controleur/SignUpFunction.php";
-
-$telephone ="0612345678";
-$nom = $email = $telephone = $prenom = $motDePasse =$pseudonyme =  "";
-
-if (isset($_POST['submit'])) {
-  echo("test");
-  $nom = test_input($_POST["nom"]); 
-  $prenom = test_input($_POST["prenom"]);
-  $motDePasse = test_input($_POST["motDePasse"]);
-  $pseudonyme = test_input($_POST["pseudonyme"]);
-  $email = test_input($_POST["email"]);
-  $telephone = test_input($_POST["telephone"]);
-
-  $validateemail = validateEmail($email);
-  $validatePhone = validateTelephone($telephone);
-
-
-  if($validateemail == true || $validatePhone == true){
-    try{
-      $hashedPassword = HashPassword($motDePasse);
-      $result = InsertUser($nom, $prenom, $pseudonyme, $email, $hashedPassword, $telephone);
-      if ($result){
-      echo "<p>Tu viens de t'inscrire comme $pseudonyme.</p>";
-    } else{
-      $errors['inscription'] = "Erreur lors de l' insertion des données.";
-  
-    }
-  }
-    catch (PDOException $e) {
-        echo "<p>Erreur : ".htmlspecialchars($e->getMessage())."</p>";
-    }
-  }
-  }
 
 $body = ob_get_clean();
 
 include_once 'views/components/template.php';
-
-
-
-
