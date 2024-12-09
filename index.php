3a9fa1f848da9f18c635eb5bfc2a3daf70936e33
Morgane Breton <morgane4478@gmail.com>
2024-12-09 10:38:42 +0100

    [FEAT]
    Modele layer for offers and requests
    Controller layer for offers and requests
    
    Some css fixes (flex -> grid)

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ $title = "Accueil";
 
 ob_start();
 
+include_once 'php/catalog/offer/getOffers.php';
+include_once 'php/catalog/request/getRequests.php';
+
+$offers = getOffers();
+$requests = getRequests();
+
 include_once 'main.html';
 
 $body = ob_get_clean();
