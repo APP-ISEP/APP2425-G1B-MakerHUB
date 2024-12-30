ecf69958de61752e54821cb457696ec6c9abede9
Mathieu GEISSEL <mathieugeissel@gmail.com>
2024-12-30 12:55:18 +0100

    [FEAT] deleting product in shopping-cart

--- shopping-cart.php
+++ shopping-cart.php
@@ -16,8 +16,13 @@ include_once("./php/shopping-cart/getProductsByUserId.php");
 $products = getProductsByUserId($_SESSION['account']['id_utilisateur']);
 
 
+if (isset($_POST) && count($_POST) > 0) {
+    include_once("./php/shopping-cart/deleteProduct.php");
 
-
+    // delete the product in the shopping-cart then refresh products array
+    $isDeleted = deleteProduct($_POST['productId'], $_SESSION['account']['id_utilisateur']);
+    $products = getProductsByUserId($_SESSION['account']['id_utilisateur']);
+}
 
 
 include_once 'views/shopping-cart.html';
