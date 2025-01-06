7252f56be7db33cec543147d5ab9b6ea4ad2b5d5
harpeliot <94528044+harpeliot@users.noreply.github.com>
2025-01-06 03:10:42 +0100

    COMMIT delete, show and search product

--- admin-product.php
+++ admin-product.php
@@ -4,8 +4,8 @@ session_start();
 $title = "Administration Product | Makerhub";
 
 ob_start();
-require('php/faq/getProducts.php'); 
-$products = getProduct();
+require('php/catalog/offer/getOffers.php'); 
+$products = getOffers();
 include_once 'views/admin-product.html';
 
 $body = ob_get_clean();
