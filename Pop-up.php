6d608a1caae795dc7a3bfc357d358c17469c3e9f
Sere04 <155636492+Sere04@users.noreply.github.com>
2025-01-06 09:36:46 +0100

    update pop up devis

--- Pop-up.php
+++ Pop-up.php
@@ -1,10 +1,15 @@
 <?php
 session_start();
 $title = "PopUP";
+$errors = array();
 
 ob_start();
 include_once 'php/catalog/offer/getOffers.php';
 include_once 'php/catalog/request/getRequests.php';
+
+if (isset($_SESSION['account'])) {
+    header("Location: index.php");
+}
 
 $offers = getOffers();
 $requests = getRequests();
@@ -24,6 +29,32 @@ else if (isset($_GET) && isset($_GET['requests-search'])) {
     $requestsSearch = $_GET['requests-search'];
     $requests = getRequests($requestsSearch);
 }
+if (isset($_POST) && count($_POST) > 0) {
+    $prixProduit = test_input($_POST["prixProduit"]);
+    $prixLivraison = test_input($_POST["prixLivraison"]);
+    $dateLivraison = test_input($_POST["dateLivraison"]);
+    $commentaire = test_input($_POST["commentaire"]);
+
+    if(!isset($_POST['prixProduit'])){
+        $errors['prixProduit'] = "Le champ est obligatoire";
+    }
+    if(!isset($_POST['prixLivraison'])){
+        $errors['prixLivraison'] = "Le champ est obligatoire";
+    }
+    if(!isset($_POST['dateLivraison'])){
+        $errors['dateLivraison'] = "Le champ est obligatoire";
+    }
+    if (empty($errors)) {
+            $result = insertDevis($prixProduit, $prixLivraison, $dateLivraison, $commentaire);
+            $account = getUser($email);
+            $_SESSION['account'] = $account;
+            $_SESSION['username'] = $account['pseudonyme'];
+            header("Location: index.php");
+        
+    }
+}
+
+
 
 include_once 'views/Pop-up_Devis.html';
 
