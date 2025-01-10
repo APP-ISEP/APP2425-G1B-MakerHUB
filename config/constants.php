8cda11f9db8b99b0620a8e83a14fd81858316e80
Morgane Breton <morgane4478@gmail.com>
2025-01-10 11:54:10 +0100

    [FIX] update PDO
    update du pdo après avoir perdu la base

--- config/constants.php
+++ config/constants.php
@@ -3,10 +3,10 @@
 
 define("PROJECT_ROOT", dirname(__DIR__));
 const LOG_FILE_PATH = PROJECT_ROOT . '/log/makerhub.log';
-const DB_HOST = 'sql3.minestrator.com';
-const DB_NAME = 'minesr_moBQkITL';
-const DB_USER = 'minesr_moBQkITL';
-const DB_PASS = 'gObeWJtKcGGA8AvS';
+const DB_HOST = 'ba64n0culhsuwcjyb9ss-mysql.services.clever-cloud.com';
+const DB_NAME = 'ba64n0culhsuwcjyb9ss';
+const DB_USER = 'u0ju2jbxdcdzmdvc';
+const DB_PASS = 'C3X9cZMufZY7DyrF6VUZ';
 const DB_PORT = '3306';
 const DB_CHARSET = 'utf8mb4';
 const FTP_HOST = 'ftp.cluster029.hosting.ovh.net';
