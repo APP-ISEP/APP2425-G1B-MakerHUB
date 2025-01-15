bb46f46ee3183da4fb62b1d8b073a35d926fd5cb
Morgane Breton <morgane4478@gmail.com>
2025-01-15 21:21:26 +0100

    [FIX] Fix de l'autoload pour les OS Linux (case sensitive)
    Signed-off-by: Morgane Breton <morgane4478@gmail.com>

--- config/autoload.php
+++ config/autoload.php
@@ -4,9 +4,23 @@ include_once "constants.php";
 
 // PENSER A INCLURE AUTOLOAD.PHP DANS CHAQUE FICHIER QUI UTILISE DES CLASSES (POO)
 
-/* Autoload */
+/* Autoload - exemple : transforme la classe Config\Ftp\FTP.php en le chemin config/ftp/FTP.php */
 spl_autoload_register(function ($class) {
-    $file = PROJECT_ROOT . '/' . str_replace('\\', '/', $class) . '.php';
+    $pathParts = explode('\\', $class);
+
+    // mettre le nom de tous les dossiers en minuscule
+    $pathPartsCount = count($pathParts);
+    for ($i = 0; $i < $pathPartsCount - 1; $i++) {
+        $pathParts[$i] = strtolower($pathParts[$i]);
+    }
+
+    // chemin relatif
+    $filePath = implode('/', $pathParts) . '.php';
+
+    // chemin absolu
+    $file = PROJECT_ROOT . '/' . $filePath;
+
+    // charge le fichier s'il existe
     if (file_exists($file)) {
         require_once $file;
     }
